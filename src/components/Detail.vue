10fc0621822013d615857617d5943f804cde3209
theseawolves <theseawolves@yeah.net>
2017-03-18 11:24:20 +0800

    finish detail page style

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -10,8 +10,16 @@
       <div class="">
       </div>
     </header>
+
+
     <section class="container">
-      <img src="" alt="" />
+      <section class="img">
+        <swipe :auto="0">
+          <swipe-item v-for="i in 3">
+            <img src="" alt="" />
+          </swipe-item>
+        </swipe>
+      </section>
       <section class="title">
         <h2>{{this.$route.query.name}}</h2>
         <div class="sale">已售：888 件</div>
@@ -25,16 +33,39 @@
         <span><i>￥</i>100</span>
         <del>￥200</del>
       </section>
-      <section>
+      <section class="ems">
         <p>
           快递： 免运费
         </p>
       </section>
-
-
     </section>
     <section class="social">
+      <div class="item">
+        <svg class="icon" aria-hidden="true">
+          <use  xlink:href='#icon-songgeiTA'></use>
+        </svg>
+        <span>送给Ta</span>
+      </div>
+      <div class="item"><span class="line"></span></div>
+      <div class="item">
+        <svg class="icon" aria-hidden="true">
+          <use  xlink:href='#icon-xihuan'></use>
+        </svg>
+        <span>收藏</span>
+      </div>
+      <div class="item"><span class="line"></span></div>
 
+      <div class="item">
+        <svg class="icon" aria-hidden="true">
+          <use  xlink:href='#icon-jiarugouwuche'></use>
+        </svg>
+        <span>加入购物车</span>
+      </div>
+      <div class="item"><span class="line"></span></div>
+
+      <div class="item">
+        <span>去天猫购买</span>
+      </div>
     </section>
     <section class="related">
       <h3>更多浪漫</h3>
@@ -57,8 +88,15 @@
 </template>
 
 <script>
+import 'vue-swipe/dist/vue-swipe.css'
+import { Swipe, SwipeItem } from 'vue-swipe'
+
 export default {
   name: 'Detail',
+  components: {
+    Swipe,
+    SwipeItem
+  },
   methods: {
     handleBack () {
       this.$router.push({path:'/'})
@@ -103,17 +141,29 @@ export default {
   background-color: white;
   padding: 0 30px;
 }
-.detail .container > img {
+.detail .container .img {
+  height: 600px;
+}
+.detail .container img {
   height: 600px;
   width: 100%;
   background-color: #ddd;
 }
+/*.detail>.container>.img>.mint-swipe>.mint-swipe-indicators>.mint-swipe-indicator.is-active {
+  background: #ddb63f;
+}*/
 .detail .container .title {
   display: flex;
   justify-content: space-between;
   align-items: center;
 }
-.detail .container .title, .detail .container .desc, .detail .container .price {
+.detail .container .desc > p {
+  line-height: 1.8;
+}
+.detail .container .ems {
+  padding-bottom: 20px;
+}
+.detail .container .title, .detail .container .desc, .detail .container .price,.detail .container .ems {
   margin-top: 15px;
 }
 .detail .container .price span, .detail .related .price span {
@@ -132,6 +182,38 @@ export default {
   color: #aaa;
   margin-left: 10px;
 }
+.detail .social {
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  border-top: 1px solid #ddd;
+  justify-content: space-between;
+  font-size: 24px;
+  color: #333;
+
+}
+.detail .social > .item {
+  display: flex;
+  align-items: center;
+}
+.detail .social > .item:last-of-type {
+  background-color: red;
+  padding: 5px 10px;
+  color: #fff;
+  font-weight: 600;
+}
+.detail .social svg {
+  height: 35px;
+  width: 35px;
+  margin-right: 15px;
+
+}
+.detail .social > .item > .line {
+  border-left: 1px solid #333;
+  line-height: 30px;
+  height: 30px;
+  width: 1px;
+}
 
 .detail .related {
   padding: 10px;
@@ -139,10 +221,11 @@ export default {
 }
 .detail .related h3 {
   padding: 0 10px;
-  font-weight: 600;
+  font-weight: 700;
   line-height: 68px;
   font-size: 30px;
   color: #333;
+  margin-bottom: 20px;
 }
 .detail .related .wrap > a.item {
   display: inline-block;
